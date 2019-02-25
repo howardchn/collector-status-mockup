15564365b18303102ae3721b282494a47aa3af96
howardchn <howardch@outlook.com>
2019-02-25 21:44:43 +0800

    add mockups

---
+++ data.js
@@ -0,0 +1,238 @@
+let data = {
+    basicInfos: [{
+            "Name": "OS",
+            "Status": "OK",
+            Details: "Microsoft Windows Server 2012 R2 Standard 64bit on VirtualBox"
+        },
+        {
+            "Name": "CPU",
+            "Status": "OK",
+            Details: "GenuineIntel: Intel(R) xxxx"
+        },
+        {
+            "Name": "Memory",
+            "Status": "OK",
+            Details: "Physical Memory:8Gb, Virtual Memory:4Gb"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "Size: Large, Maximum Memory:4Gb, Version: 27005"
+        },
+        {
+            "Name": "Free Memory",
+            "Status": "Warn",
+            Details: "Memory is not enough: Free memory is only 280Mb which is not enough for Agent reaching maximum memory"
+        }
+    ],
+    moduleState: [{
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        }, {
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        }
+    ],
+    eventCollecting: [{
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        },{
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        }
+    ],
+    dataCollecting: [{
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        },{
+            "Name": "Watchdog",
+            "Status": "OK",
+            Details: "has running 2 days"
+        },
+        {
+            "Name": "Agent",
+            "Status": "OK",
+            Details: "1 restart/day"
+        },
+        {
+            "Name": "Sbproxy",
+            "Status": "Error",
+            Details: "has Down 30 minutes"
+        },
+        {
+            "Name": "SSE",
+            "Status": "Warn",
+            Details: "Restart too frequency, 10 restarts/hour"
+        },
+        {
+            "Name": "SPSE",
+            "Status": "Disabled",
+            Details: "Disabled in the agent.conf "
+        },
+        {
+            "Name": "Netflow",
+            "Status": "OK",
+            Details: "Listening on port 2055, and monitoring 100 devices"
+        },
+        {
+            "Name": "Reporter",
+            "Status": "OK",
+            Details: "PersistentQueue enabled, latency is 100ms"
+        }
+    ]
+}
